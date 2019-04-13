e6fad464723e381ad8f5023561448e8fc5626573
ניסן אגבייב <bknisan@gmail.com>
2019-04-13 11:30:22 +0300

    starting to work on buttons

--- Ex2/App.xaml.cs
+++ Ex2/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Ex2
+namespace FlightSimulator
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Ex2/MainWindow.xaml.cs
+++ Ex2/MainWindow.xaml.cs
@@ -10,10 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Ex2
+namespace FlightSimulator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
